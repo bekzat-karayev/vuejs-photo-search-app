2f623e5e808f8dd5b265debf992de042cece003f
Bekzat Karayev <bekzatkarayev@gmail.com>
2023-07-15 10:25:56 +0600

    Set up store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-    Home
+    <pre>{{ photos }}</pre>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import store from '../store';
+
+
+const photos = computed(() => store.state.photos)
 
 </script>
